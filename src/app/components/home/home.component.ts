4e0aad0e647da41c8b063df655ca3de1061fdb73
Albert Font <albert.font.fernandez@gmail.com>
2020-12-10 17:10:48 +0100

    token auto

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,20 +5,24 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
+  styleUrls: [
+    './home.component.scss'
   ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   nuevasCanciones: any[] = [];
 
   constructor(private spotify:SpotifyService) {
+    
+  }
+
+  async ngOnInit(){
+    await this.spotify.init()
     this.spotify.getNewReleases()
     .subscribe((data :any)=>{
       this.nuevasCanciones = data.albums.items;
       console.log(this.nuevasCanciones)
     });
   }
-    
-  
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,35 +1,59 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class SpotifyService {
+export class SpotifyService{
 
-  token = "BQBVa9aV4-9xTg30TpBl46ka94pstipyPHFV77SxNynh1sNNcK_6MyG2CdPyjuxQdCfwT4FaDTUFM99k440qwyS3bM1xqkom3Hp8wHVgStwVLlFru25uGEUhN13XCypfSJcn_27b_P64TQ"
+  client_id:string = "96f4df85445444fab0a297c62ea3151e";
+  client_secret:string = "cde7a89d829940d08f2a2b27f874b0f3";
+
+  token:any = "";
 
   constructor(private http:HttpClient) {
-    console.log("Spotify Service Ready")
-   }
+    
+  }
 
-   
-   getNewReleases(){
+  async init(){
+    this.token = await this.getToken(this.client_id, this.client_secret)
+    console.log(this.token)
+  }
 
+  async getToken(client_id:string, client_secret:string) : Promise<Object>{
+    // Variables
+    let token_url = "https://accounts.spotify.com/api/token"
+    let httpParams = new HttpParams().append("grant_type", "client_credentials");
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Authorization": "Basic " + btoa((client_id + ":" + client_secret))
+      })
+    };
+
+    let self:any = this;
+
+    // Petició rest
+    return (new Promise(function(resolve, reject){
+      self.http.post(token_url, httpParams, httpOptions).subscribe((data : any)=>{
+        resolve(data["access_token"])
+      })
+    }))
+  }
+  
+  // Request per obtenir les noves cançons
+  getNewReleases(){
     const headers = new HttpHeaders({
      "Authorization": "Bearer " + this.token
     })
-    
-    return (this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers}))
-    
-    
+    return (this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=10', {headers}))
    }
 
+   // Request per buscar l'artista
    getArtista( termino:string ){
     const headers = new HttpHeaders({
       "Authorization":"Bearer " + this.token
      })
-     
      return (this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=5`, {headers}))
    }
 }
